1a8805e7fedb7c046a46dc3e2ad590c6066dbeb0
Ferens Dmitriy <ferensdima@gmail.com>
2014-09-25 11:58:12 +0300

    Added 'lein-haml-sass' plugin

--- project.clj
+++ project.clj
@@ -8,16 +8,21 @@
                  [reagent "0.4.2"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-haml-sass "0.2.7-SNAPSHOT"]
             [lein-figwheel "0.1.4-SNAPSHOT"]
             [lein-bower "0.5.1"]]
 
   :bower-dependencies [[react "0.9.0"]]
 
   :source-paths ["src"]
+  :sass {:src "resources/public/sass"
+         :output-directory "resources/public/css"
+         :output-extension "css"}
 
   :cljsbuild {:builds [{:id "bomberman"
                         :source-paths ["src"]
                         :compiler {:output-to "resources/public/js/compiled/bomberman.js"
                                    :output-dir "resources/public/js/compiled/out"
                                    :optimizations :none
-                                   :pretty-print true}}]})
+                                   :pretty-print true}}]}
+  :figwheel {:css-dirs ["resources/public/css"]})
